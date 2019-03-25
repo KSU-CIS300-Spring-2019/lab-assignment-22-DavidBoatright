3f0967c467714f4c5f95719c314a027a551f9db2
RBOATRIGHT46@GMAIL.COM <RBOATRIGHT46@GMAIL.COM>
2019-03-25 15:13:13 -0500

    completed lab

---
+++ Ksu.Cis300.TrieLibrary/TrieWithNoChildren.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    public class TrieWithNoChildren : ITrie
+    {
+        /// <summary>
+        /// stores if the trie is the end of the word
+        /// </summary>
+        private bool _isEmpty = false;
+
+        /// <summary>
+        /// adds a word
+        /// </summary>
+        /// <param name="s">the woprd to be added</param>
+        /// <returns>the new trie</returns>
+        public ITrie Add(string s)
+        {
+            if (s == "")
+            {
+                _isEmpty = true;
+            }
+            else
+            {
+                return new TrieWithOneChild(s, _isEmpty);
+            }
+            return this;
+        }
+        /// <summary>
+        /// searches for a word
+        /// </summary>
+        /// <param name="s">the word to be found</param>
+        /// <returns>if the word was found</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _isEmpty;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Ksu.Cis300.TrieLibrary/TrieWithOneChild.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TrieLibrary
+{
+    class TrieWithOneChild : ITrie
+    {
+        /// <summary>
+        /// stores if the trie is the end of the word
+        /// </summary>
+        private bool _isEmpty;
+
+        /// <summary>
+        /// the child of the trie
+        /// </summary>
+        private ITrie _child;
+        
+        /// <summary>
+        /// The child's label
+        /// </summary>
+        private char _label;
+        /// <summary>
+        /// constructs a new trie with one child
+        /// </summary>
+        /// <param name="s">the given word</param>
+        /// <param name="isEmpty">if the trie is the end of the word</param>
+        public TrieWithOneChild(string s, bool isEmpty)
+        {
+            if (s[0] < 'a' || s[0] > 'z' || s.Equals(""))
+            {
+                throw new ArgumentException();
+            }
+            _isEmpty = isEmpty;
+            _label = s[0];
+            _child = new TrieWithNoChildren();
+            _child = _child.Add(s.Substring(1));
+        }
+
+        /// <summary>
+        /// Adds a word
+        /// </summary>
+        /// <param name="s">String to be added</param>
+        /// <returns>the new trie</returns>
+        public ITrie Add(string s)
+        {
+
+            if (s == "")
+            {
+                _isEmpty = true;
+            }
+            else if (_label == s[0])
+            {
+                _child = _child.Add(s.Substring(1));
+                return this;
+            }
+            else if (_label != s[0])
+            {
+                return new TrieWithManyChildren(s, _isEmpty, _label, _child);
+            }
+            return this;
+        }
+        /// <summary>
+        /// looks for a given word
+        /// </summary>
+        /// <param name="s">the word to be found</param>
+        /// <returns>if the word was found</returns>
+        public bool Contains(string s)
+        {
+            if (s == "")
+            {
+                return _isEmpty;
+            }
+            else if (s[0] == _label)
+            {
+                return _child.Contains(s.Substring(1));
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
